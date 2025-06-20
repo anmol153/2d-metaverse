e420254338a61bc05f427e6cd36694ef0c82a56a
AnmolDhima12 <anmoldhimans525@gmail.com>
2025-06-20 21:50:30 +0530

    change for deployment

--- 2d_metaverse/api/index.js
+++ 2d_metaverse/api/index.js
@@ -25,12 +25,13 @@ app.use("/api/v1/message", MessageRoute);
 app.use("/api/v1/live",router)
 app.use(errorHandler);
 
-if(process.env.NODE_ENV==="production"){
+
+
   app.use(express.static(path.join(__dirname,"../dist")));
-  app.get("*",(req,res)=>{
+  app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"../","dist","index.html"));
   })
-}
+
 
 connectDB()
   .then(() => {
